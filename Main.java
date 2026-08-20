2a041b63cf5ac7c4d1453316219554d2d1e47e30
Albert Adekanye <74176768+AlbertAdekanye@users.noreply.github.com>
2026-08-20 14:43:34 +0100

    Add initial Java class and VSCode settings for debugging

---
+++ Main.java
@@ -0,0 +1,11 @@
+// Primitive types
+public class Main {
+    public static void main(String[] args) {
+        byte age = 24;
+        long viewsCount = 3_123_456_789L;
+        float price = 10.99F;
+        char letter = 'A';
+        boolean isEligible = true;
+        // System.out.println(myAge);
+    }
+}
