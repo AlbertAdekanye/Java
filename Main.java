306e7f504c12e47e0ee6f031ba76aa822262dffa
Albert Adekanye <74176768+AlbertAdekanye@users.noreply.github.com>
2026-08-20 15:00:32 +0100

    Refactor Main class to remove unused primitive type variables and add current date display

--- Main.java
+++ Main.java
@@ -1,11 +1,24 @@
+
+import java.util.Date;
+
 // Primitive types
+// public class Main {
+//     public static void main(String[] args) {
+//         byte age = 24;
+//         long viewsCount = 3_123_456_789L;
+//         float price = 10.99F;
+//         char letter = 'A';
+//         boolean isEligible = true;
+//         // System.out.println(myAge);
+//     }
+// }
+
+
+// reference types
 public class Main {
     public static void main(String[] args) {
         byte age = 24;
-        long viewsCount = 3_123_456_789L;
-        float price = 10.99F;
-        char letter = 'A';
-        boolean isEligible = true;
-        // System.out.println(myAge);
+        Date now = new Date();
+        System.out.println(now);
     }
 }
