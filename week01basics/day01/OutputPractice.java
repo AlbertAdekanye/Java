688e3a3960b85f708f0a2f6ddfdab40e6775bfc4
Albert Adekanye <74176768+AlbertAdekanye@users.noreply.github.com>
2026-08-23 00:06:39 +0100

    Add OutputPractice class to demonstrate basic output functionality

---
+++ week01basics/day01/OutputPractice.java
@@ -0,0 +1,12 @@
+package week01basics.day01;
+
+public class OutputPractice {
+    public static void main(String[] args) {
+        System.out.println("Cristiano Ronaldo");
+        System.out.println("Number 7");
+
+        System.out.print("Siuuu ");
+        System.out.print("Siuuu ");
+        System.out.print("Siuuu");
+    }
+}
