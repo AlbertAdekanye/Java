4dbc605fe96a94a96f85a55167c41a69e75ffc3f
Albert Adekanye <74176768+AlbertAdekanye@users.noreply.github.com>
2026-08-24 22:35:42 +0100

    Add ArithmeticOperators class to demonstrate basic arithmetic operations

---
+++ week01basics/day02/ArithmeticOperators.java
@@ -0,0 +1,15 @@
+package week01basics.day02;
+
+public class ArithmeticOperators {
+    public static void main(String[] args) {
+
+        int firstNumber = 10;
+        int secondNumber = 3;
+
+        System.out.println("Addition: " + (firstNumber + secondNumber));
+        System.out.println("Subtraction: " + (firstNumber - secondNumber));
+        System.out.println("Multiplication: " + (firstNumber * secondNumber));
+        System.out.println("Division: " + (firstNumber / secondNumber));
+        System.out.println("Remainder: " + (firstNumber % secondNumber));
+    }
+}
