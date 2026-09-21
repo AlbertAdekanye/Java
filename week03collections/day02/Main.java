5a94c914b021b1cb9e7d01f469a6c447af7f291f
Albert Adekanye <74176768+AlbertAdekanye@users.noreply.github.com>
2026-09-21 23:42:36 +0100

    Enhance Main class: add logic for checking presence and emptiness of HashSet, and improve number handling

--- week03collections/day02/Main.java
+++ week03collections/day02/Main.java
@@ -8,12 +8,42 @@ public class Main {
       
     HashSet<String> names = new HashSet<>();
 
+    HashSet<Integer> numbers = new HashSet<>();
+
     names.add("Albert");
     names.add("Adesoye");
     names.add("Adekanye");
+    //Duplicate values
+    names.add("Albert");
+    names.add("Albert");
+    names.add("Albert");
 
     names.add("Jea");
+    names.remove("Albert");
     System.out.println(names);
     System.out.println(names.size());
+
+    if (names.contains("Adekanye")) {
+      System.out.println("Present");
+    } if (names.contains("Albert")) {
+      System.out.println("present");
+    } else {
+      System.out.println("Absent");
+    }
+
+    if (names.isEmpty()) {
+      System.out.println("The set is empty.");
+    }
+
+    System.out.println();
+    // HashSet with numbers
+    numbers.add(24);
+    numbers.add(24);
+    numbers.add(23);
+    numbers.add(22);
+    numbers.add(21);
+    numbers.add(20);
+
+    System.out.println(numbers);
   }
 }
