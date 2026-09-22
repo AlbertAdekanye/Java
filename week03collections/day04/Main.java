056063373587c0a3a660676a08df78fe230a27ea
Albert Adekanye <74176768+AlbertAdekanye@users.noreply.github.com>
2026-09-22 13:59:17 +0100

    Add Main class: implement basic functionality to display a list of names

---
+++ week03collections/day04/Main.java
@@ -0,0 +1,20 @@
+package week03collections.day04;
+
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        ArrayList<String> names = new ArrayList<>();
+
+        names.add("Albert");
+        names.add("Adesoye");
+        names.add("John");
+        names.add("David");
+
+        for (int i = 0; i < names.size(); i++) {
+            System.out.println(names.get(i));
+        }
+    }
+}
