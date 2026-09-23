8fdc547ead3a1f09ed0dd50172cd9bc5fcfff9f5
Albert Adekanye <74176768+AlbertAdekanye@users.noreply.github.com>
2026-09-23 07:42:47 +0100

    Enhance Main class: add a new name to the list of names displayed

--- week03collections/day04/Main.java
+++ week03collections/day04/Main.java
@@ -12,9 +12,11 @@ public class Main {
         names.add("Adesoye");
         names.add("John");
         names.add("David");
+        names.add("King");
 
         for (int i = 0; i < names.size(); i++) {
             System.out.println(names.get(i));
         }
+
     }
 }
